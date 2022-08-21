5cd29c50abe8d4e7fe34e38523a7b80f57da781c
Raphael Kauan <111379005+raphaelkauanf@users.noreply.github.com>
2022-08-21 00:18:49 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,13 @@ const mario = document.querySelector('.mario');
 
 const jump = () => {
     mario.classList.add('jump');
+
+    setTimeout( () => {
+
+        mario.classList.remove('jump');
+
+
+  }, 500);
 }
 
 document.addEventListener('keydown' , jump)
