7ba3c942fffc6a6edbe50c71cc86b9cd42bbd724
Gabriel Lucas <69332675+gabrielgt5@users.noreply.github.com>
2022-08-21 01:11:13 -0400

    update final

--- script.js
+++ script.js
@@ -29,6 +29,8 @@ const loop = setInterval(() => {
     mario.src = '/img/game-over.png';
     mario.style.width = '75px';
     mario.style.marginLeft = '50px' ; 
+    
+    clearInterval(loop);
   }
 
  
